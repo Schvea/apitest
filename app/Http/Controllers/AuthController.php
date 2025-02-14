0c1cde23f042ce5bb8bac7eae021a201248c4d31
scvhea <svea.holm03@gmail.com>
2025-02-14 10:19:19 +0100

    controller auth function register

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,8 +3,26 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Foundation\Auth\User;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
-    //
+    public function register(Request $request){
+        try {
+            $request->validate([
+                'name' => 'required',
+                'email' => 'required|email|unique:users',
+                'password' => 'required|confirmed|min:8',
+            ]);
+            User::create([
+                'name' => $request->name,
+                'email' => $request->email,
+                'password' => Hash::make($request->password)
+            ]);
+            return response()->json(['message' => 'New user registered'], 201);
+        } catch(\Exception $e){
+            return response()->json(['message' => 'moo'], 400);
+        }
+    }
 }
