50ab7fef0ffe55faa6bdc36a1f663297b91ea10b
scvhea <svea.holm03@gmail.com>
2025-02-16 18:55:34 +0100

    metod chat

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,9 +2,10 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use App\Models\User;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
 
 class AuthController extends Controller
 {
@@ -25,4 +26,29 @@ class AuthController extends Controller
             return response()->json(['message' => $e->getMessage()], 400);
         }
     }
+
+public function login(Request $request) {
+    try {
+
+        $user = User::where('email', $request->email)->first();
+
+        if (!$user || !Hash::check($request->password, $user->password)) {
+            throw ValidationException::withMessages([
+                'message' => ['fel inlog'],
+            ]);
+        }
+
+        $token = $user->createToken('accessToken');
+
+        return response()->json(['accessToken' => $token->plainTextToken], 200);
+
+    } catch(\Exception $e) {
+
+        return response()->json(['message' => $e->getMessage()], 401);
+    }
+}
+public function logout (Request $request){
+    $request->user()->tokens()->delete();
+    return response()->json(['message' => 'logged out'], 200);
+}
 }
